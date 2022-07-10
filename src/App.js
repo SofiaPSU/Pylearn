89e879ed02121465e18047f1f9b5ce2a710df701
SofiaPSU <sxr5616@psu.edu>
2022-07-10 15:29:16 -0400

    Changed HelloWord to HelloWorld and added text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./home";
 import Navbar from "./navbar";
-import HelloWorld from "./HelloWord";
+import HelloWorld from "./HelloWorld";
 //import Debug from "./debug";
 
 
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -4,8 +4,11 @@ import React from 'react';
 
 export default function HelloWorld(){
     return ( 
-  
+            <div>
+                <p> Printing in Python</p>
+          
             <iframe src="https://trinket.io/embed/python/c8a347c04f" width="500" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen
-            title="print"></iframe>      
+            title="print"></iframe>   
+            </div>   
     );
 }
