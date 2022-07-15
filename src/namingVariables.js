c6152fcada1746f51abb15641d7a8c3e5bb8e5c4
Shivani <skj5505@psu.edu>
2022-07-15 11:25:09 -0400

    Added a Trinket embed to namingVariables

---
+++ src/namingVariables.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export default function namingVariables(){
+    return ( 
+            <div>
+                <p> Naming Variables in Python</p>
+          
+            <iframe src="https://trinket.io/embed/python/b08da7c17c" width="500" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen
+            title="print"></iframe>   
+            </div>   
+    );
+}
