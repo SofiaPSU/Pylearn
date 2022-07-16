b7c3c65409f19b45951016b6f385c5da0dcd3c50
Shivani <skj5505@psu.edu>
2022-07-15 20:09:48 -0400

    Created the indentation sensitivity page w/ CSS

--- src/namingVariables.js
+++ src/namingVariables.js
@@ -12,7 +12,7 @@ export default function namingVariables(){
             <div id = "col1" align = "left">
             <p>1. Variable names must start with a letter of underscore.</p>
             <p>2. Variable names cannot start with a number or special character.</p>
-            <p>1. Variable names can only contain alphanumeric values and underscores.</p>
+            <p>3. Variable names can only contain alphanumeric values and underscores.</p>
             </div>
             <div>
             <iframe src="https://trinket.io/embed/python/b08da7c17c" width="500" height="500" align = "right" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen
