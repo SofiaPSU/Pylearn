24d9e14fd356d5702501457cb16bdfdc50766326
Taylor <taylorkp22@gmail.com>
2022-07-18 18:52:25 -0400

    strings js and css coded

---
+++ src/strings.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './strings.css';
+import Button from '@mui/material/Button';
+
+
+
+export default function strings(){
+    return ( 
+            <div>
+                <p> Strings in Python</p>
+
+            <div class = "grid-container">
+            <div id = "col1" align = "left">
+            <p>1. Strings can contain alphanumerical characters as well as special characters.</p>
+            <p>2. Python uses ' ', " ", and ''' ''' to represent string literals.</p>
+            </div>
+            <div>
+            <iframe src="https://trinket.io/embed/python/51162e759d" width="500" height="500" align = "right" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen
+            title="print"></iframe>   
+            </div>
+            </div>
+
+            <Button variant="contained" href="variableDef.js">
+            Next
+            </Button>
+    
+            </div>   
+    );
+}
