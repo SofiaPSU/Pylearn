b590a6af8b21bebc9b2b9d40617c40753a306aad
Shivani <skj5505@psu.edu>
2022-07-19 21:59:57 -0400

    Debug Tips page

---
+++ src/debug.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './debug.css';
+import Button from '@mui/material/Button';
+import {Link} from 'react-router-dom';
+
+
+export default function debug(){
+    return ( 
+            <div>
+                <p> Debugging Tips</p>
+                <br></br>
+            <div id = "tips" align = "left">
+            <p>1. Take a break from your code and clear your mind before you come back to it.</p>
+            <p>2. Don’t be afraid to ask someone for help (office hours are a great resource!) </p>
+            <p>3. Explain your code to someone else or talk through it. </p>
+            <p>4. Draw out your code.</p>
+            <p>5. Go line by line and check the actual function of each line. </p>
+            <p>6. Look for syntax errors and check the Terminal explanation. </p>
+            <p>7. Using print and variables in between your code to check until where it works correctly. </p>
+            <p>8. Check your variable names and assignment operators. </p>
+            </div>
+
+<br></br>
+            <Button variant="contained">
+            
+            <Link className="next" to="/commenting"> Continue </Link>
+            </Button>
+            
+            </div>   
+    );
+}
