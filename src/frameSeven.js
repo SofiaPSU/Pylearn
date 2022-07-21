f523219cbfff4880834f0c47d348da2f73c464d8
Shivani <skj5505@psu.edu>
2022-07-21 12:39:15 -0400

    Alignment of frameSeven

--- src/frameSeven.js
+++ src/frameSeven.js
@@ -8,22 +8,22 @@ import { Link } from 'react-router-dom';
 export default function frameSeven(){
     return ( 
             <div>
-                
+                <br></br>
+                <p>Debugging Practice #1</p>
 
-            <div class = "grid-container">
-            <div id = "col1" align = "left">
+            <center>
+            <iframe src="https://trinket.io/embed/python/969b166814" width="500" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen
+            title="print"></iframe> 
             <p>__ __    __ __ __    __ __ __</p>
-            </div>
-            <div>
-            <iframe src="https://trinket.io/embed/python/969b166814" width="500" height="500" align = "right" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen
-            title="print"></iframe>   
-            </div>
-            </div>
+            </center>
+            
 
             <Button variant="contained" >
             <Link className="next" to="/HelloWorld"> Continue </Link>
             </Button>
+            <br></br>
+            <br></br>
+            </div>
     
-            </div>   
     );
 }
