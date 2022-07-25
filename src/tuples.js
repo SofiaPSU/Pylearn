55317501eb2100692cf3cf0d80e7f5d5c6b35f71
Shivani <skj5505@psu.edu>
2022-07-25 16:18:52 -0400

    tuples Page update

--- src/tuples.js
+++ src/tuples.js
@@ -17,15 +17,14 @@ export default function tuples() {
     return (
         <div>
             <br></br>
-            <p> Tuples, Lists, and Dictionaries </p>
+            <p> Lists and Dictionaries </p>
             <Grid container spacing={12} wrap="nowrap" className='center'>
                 <MyGrid item xs={6} align="left"  >
 
                     <p>These data types can be defined using different brackets.</p>
-                    <p>1. Tuples - (1,2,3)</p>
-                    <p>2. Lists - [1,2,3]</p>
-                    <p>3. Dictionaries - {"{"}1:'a',2:'b',3:'c'{"}"}</p>
-                    <p>Both tuples and lists can be referenced by index values. Dictionaries are unordered and referenced by their keys.</p>
+                    <p>1. Lists - [1,2,3]</p>
+                    <p>2. Dictionaries - {"{"}1:'a',2:'b',3:'c'{"}"}</p>
+                    <p>Lists can be referenced by index values. Dictionaries are unordered and referenced by their keys.</p>
 
                 </MyGrid>
                 <Grid item xs={6} paddingRight={10} >
